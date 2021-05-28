a2a62038df815d82ec91468742d123eaecefe278
Lasota <anna.a.lasota@accenture.com>
2021-05-28 15:30:44 +0200

    task1 done

---
+++ task_1/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "kotlet-vm"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+}
